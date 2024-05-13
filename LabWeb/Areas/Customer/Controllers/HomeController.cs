73366e44726afc1b007ec95da54f586347103490
jnana <jnanaranjansahoo016@gmail.com>
2024-05-13 17:06:58 +0530

    Exp

--- LabWeb/Areas/Customer/Controllers/HomeController.cs
+++ LabWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,24 +1,114 @@
+using Lab.DataAcess.Repository;
+using Lab.DataAcess.Repository.IRepository;
 using Lab.Models;
+using Lab.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace LabWeb.Areas.Customer.Controllers
 {
     [Area("Customer")]
+    
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUnitOfWork _unitOfWork;
+        public HomeController(ILogger<HomeController> logger , IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
             return View();
         }
+ 
+        public IActionResult Appointment()
+        {
+            List<Appointment> objAppointmentList = _unitOfWork.Appointment.GetAll().ToList();
 
+            return View(objAppointmentList);
+        }
+        [Authorize]
+        public IActionResult Upsert(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                //Create
+
+                return View(new Appointment());
+            }
+            else
+            {
+                //update
+
+                Appointment appointmenObj = _unitOfWork.Appointment.Get(u => u.Id == id);
+                return View(appointmenObj);
+            }
+        }
+       
+        [HttpPost]
+        public IActionResult Upsert(Appointment AppointmentObj)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                if (AppointmentObj.Id == 0)
+                {
+                    _unitOfWork.Appointment.Add(AppointmentObj);
+                }
+                else
+                {
+                    _unitOfWork.Appointment.Update(AppointmentObj);
+                }
+
+                _unitOfWork.Save();
+                TempData["success"] = "Appointment Created Successfully";
+                return RedirectToAction("Appointment");
+            }
+            else
+            {
+                return View(AppointmentObj);
+            }
+        }
+        
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Appointment? appointmentFromDb = _unitOfWork.Appointment.Get(u => u.Id == id);
+            if (appointmentFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(appointmentFromDb);
+
+        }
+       
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+
+            var appointmentToBeDeleted = _unitOfWork.Appointment.Get(u => u.Id == id);
+            if (appointmentToBeDeleted == null)
+            {
+                return Json(new { success = false, Message = "Error while deleting" });
+            }
+
+            _unitOfWork.Appointment.Remove(appointmentToBeDeleted);
+            _unitOfWork.Save();
+
+            TempData["success"] = "Appointment Deleted Successfully";
+
+            return RedirectToAction("Appointment");
+
+        }
         public IActionResult AboutUs()
         {
             return View();
