9292d5b583550d90b0899441fc762e716363e8bf
jnana <jnanaranjansahoo016@gmail.com>
2024-05-15 11:03:17 +0530

    chanf

---
+++ LabWeb/Areas/OfficerView/Controllers/OfficerViewController.cs
@@ -0,0 +1,20 @@
+﻿using Lab.DataAcess.Repository.IRepository;
+using Lab.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabWeb.Areas.OfficerView.Controllers
+{
+    public class OfficerViewController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OfficerViewController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Client> objClientList = _unitOfWork.Client.GetAll(includeProperties:"Officer");
+            return View(objClientList);
+        }
+    }
+}
