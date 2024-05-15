f870b8774025ca6e3ed639331d758a6a7b8d2c3d
jnana <jnanaranjansahoo016@gmail.com>
2024-05-15 10:10:22 +0530

    offcer

---
+++ LabWeb/Areas/Officer/Controllers/OfficerViewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LabWeb.Areas.Officer.Controllers
+{
+    public class OfficerViewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
