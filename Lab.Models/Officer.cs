ba2d966cd7744a925463696e2b0ffb534c429df3
jnana <jnanaranjansahoo016@gmail.com>
2024-05-22 16:24:38 +0530

    w

--- Lab.Models/Officer.cs
+++ Lab.Models/Officer.cs
@@ -10,7 +10,25 @@ namespace Lab.Models
         [Display(Name = "Enter Name")]
         public string? Name { get; set; }
 
-        [Display(Name = "Costumor No.")]
-        public int? Cost { get; set; }
+        [Display(Name = "Enter Name")]
+        public int? Totalclient { get; set; }
+
+        [Required]
+        [Display(Name = "Mobile No.")]
+
+        public int? Mobile { get; set; }
+
+        [Required]
+        [Display(Name = "District")]
+        public string? Dist { get; set; }
+        [Required]
+        [Display(Name = "Post")]
+        public string? Pos { get; set; }
+        [Required]
+        [Display(Name = "Pin Code")]
+        public int? Pin { get; set; }
+        [Required]
+        [Display(Name = "Land Mark")]
+        public string? LandMark { get; set; }
     }
 }
